c89d578df801508c7277511ca8e722cb5cef0980
Diego <Petronacidiego@gmail.com>
2020-11-30 17:52:50 +0100

    esercizio finito ma da rifare in modo più "confort"

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,52 @@
 
 $(function(){
 
-    var tariffazione = $(".tariffazione");
+    var Tariffazione = $("#Tariffazione");
+    Tariffazione.mouseenter(function () {
+        $(".link_uno").removeClass("invisible");
+    });
+    Tariffazione.mouseout(function () {
+        $(".link_uno").addClass("invisible");
+    });
 
-    tariffazione.mouseEnter(function () {
-        $("this").removeClass("invisible");
+    var Conti = $("#Conti");
+    Conti.mouseenter(function () {
+        $(".link_due").removeClass("invisible");
+    });
+    Conti.mouseout(function () {
+        $(".link_due").addClass("invisible");
+    });
+
+    var Prodotti = $("#Prodotti");
+    Prodotti.mouseenter(function () {
+        $(".link_tre").removeClass("invisible");
+    });
+    Prodotti.mouseout(function () {
+        $(".link_tre").addClass("invisible");
+    });
+
+    var Azienda = $("#Azienda");
+    Azienda.mouseenter(function () {
+        $(".link_quattro").removeClass("invisible");
+    });
+    Azienda.mouseout(function () {
+        $(".link_quattro").addClass("invisible");
+    });
+
+    var Aiuto = $("#Aiuto");
+    Aiuto.mouseenter(function () {
+        $(".link_cinque").removeClass("invisible");
+    });
+    Aiuto.mouseout(function () {
+        $(".link_cinque").addClass("invisible");
+    });
+
+    var It = $("#It");
+    It.mouseenter(function () {
+        $(".link_sei").removeClass("invisible");
+    });
+    It.mouseout(function () {
+        $(".link_sei").addClass("invisible");
     });
 
 });
